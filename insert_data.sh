ea71288e2306da4385d6430f74588b55892dc4cf
Nicolae Adelin <nicolae.adelin30@gmail.com>
2026-04-07 09:25:04 -0400

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 if [[ $1 == "test" ]]
 then
@@ -7,4 +7,51 @@ else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 fi
 
-# Do not change code above this line. Use the PSQL variable above to query your database.
+# Ștergem tot ce e în tabele ca să o luăm de la zero la fiecare rulare
+echo $($PSQL "TRUNCATE games, teams")
+
+# Citim games.csv folosind while loop
+# IFS="," ne ajută să separăm coloanele automat
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_G OPPONENT_G
+do
+  # Sărim peste prima linie (header)
+  if [[ $YEAR != "year" ]]
+  then
+    # 1. GESTIONARE ECHIPA CÂȘTIGĂTOARE
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    
+    # Dacă nu e găsită, o inserăm
+    if [[ -z $WINNER_ID ]]
+    then
+      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      if [[ $INSERT_WINNER_RESULT == "INSERT 0 1" ]]
+      then
+        echo "Inserted into teams: $WINNER"
+      fi
+      # Luăm ID-ul nou creat
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    fi
+
+    # 2. GESTIONARE ECHIPA ADVERSARĂ
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    
+    # Dacă nu e găsită, o inserăm
+    if [[ -z $OPPONENT_ID ]]
+    then
+      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      if [[ $INSERT_OPPONENT_RESULT == "INSERT 0 1" ]]
+      then
+        echo "Inserted into teams: $OPPONENT"
+      fi
+      # Luăm ID-ul nou creat
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    fi
+
+    # 3. INSERARE MECI
+    INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_G, $OPPONENT_G)")
+    if [[ $INSERT_GAME_RESULT == "INSERT 0 1" ]]
+    then
+      echo "Inserted game: $YEAR $ROUND"
+    fi
+  fi
+done
